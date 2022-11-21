7ee4d5a9039a8b674d7780f7fe5f04139c20f503
Raiyan Aaijaz <raiyanjaz@gmail.com>
2022-11-20 23:31:00 -0500

    Commented on createDataMatrix and loadRFData

--- amode.cpp
+++ amode.cpp
@@ -10,42 +10,42 @@ using namespace std;
 
 float **createDataMatrix(int numElement, int numSample) {
     // Create a 2D data matrix of size numElement and numSample
-    float** RFData;
-    RFData = new float*[numElement];
+
+    float** RFData; // Initializes a double pointer
+    RFData = new float*[numElement]; // Allocates blocks of pointer elements (numElement length)
     for (int i = 0; i < numElement; i++)
-        RFData[i] = new float[numSample];
+        RFData[i] = new float[numSample]; // For every element, allocates a numElement number of blocks to each pointer element
     return RFData;
 }
 
 int loadRFData(float **RFData, const char *fileName, int numElement, int numSample) {
     // Open the text file fileName, read the data and store into RFData
     // You can use the getline() command to extract the data lines from the txt files
+
     ifstream infile(fileName); //Opens file
 
-    if (infile.fail()) { //Checks if file is open
+    if (infile.fail()) { // Returns -1 to main if file cannot be open
         return -1;
     }
 
     const int MAX = 100;
     char line[100];
-    int elementCounter = 0;
+    int elementCounter = 0; // Counters to be used in the while loop
     int sampleCounter = 0;
 
-    while (infile.getline(line, MAX)) {
-        RFData[elementCounter][sampleCounter] = atof(line);
+    while (infile.getline(line, MAX)) { // Runs until end of file (EOF) returns false
+        RFData[elementCounter][sampleCounter] = atof(line); // Converts char array to float and stores in RFData
         sampleCounter++;
         if (sampleCounter == numSample) { // Every 3338 samples, element counter is increased by 1.
             elementCounter++;
             sampleCounter = 0;
         }
-        if (elementCounter == numElement) {
-            break;
-        }
     }
+    infile.close(); // Closes file
 }
 
-// Create an array containing the depth location (in z-direction) for each pixel on the scanline
 float *genScanlineLocation(int &numPixel) {
+// Create an array containing the depth location (in z-direction) for each pixel on the scanline
     float depth;
     int pixels;
     cout << "Enter scanline depth (>0): ";
@@ -112,7 +112,6 @@ float *elementPosition, int numElement, int numSample, int numPixel, float FS, f
         pImag[i] = sum;
         sum = 0;
     }
-    
 }
 
 // Write the scanline to a csv file
